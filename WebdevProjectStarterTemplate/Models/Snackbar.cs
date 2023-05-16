8dc6213b9d07f2a6f0bd34c1ed965cb5851a1cd3
Sander Burger <sander.burger@student.nhlstenden.com>
2023-05-16 10:45:28 +0200

    Snackbar Model en Repo

---
+++ WebdevProjectStarterTemplate/Models/Snackbar.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebdevProjectStarterTemplate.Models
+{
+    public class Snackbar
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Naam { get; set; }
+    }
+}
