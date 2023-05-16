a2791d6e5ff015f236c1e9b2e077fac3f80ed950
Sander Burger <sander.burger@student.nhlstenden.com>
2023-05-16 11:11:31 +0200

    SnackbarDelete

--- WebdevProjectStarterTemplate/Pages/Snackbars/Delete.cshtml.cs
+++ WebdevProjectStarterTemplate/Pages/Snackbars/Delete.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebdevProjectStarterTemplate.Models;
+using WebdevProjectStarterTemplate.Repositories;
 
 namespace WebdevProjectStarterTemplate.Pages.Snackbars
 {
-    public class DeleteModel : PageModel
+    public class Delete : PageModel
     {
-        public void OnGet()
+        public Snackbar Snackbar { get; set; } = null!;
+
+        public void OnGet([FromRoute] int snackbarId)
         {
+            Snackbar = new SnackbarRepository().Get(snackbarId);
+        }
+
+        public IActionResult OnPostDelete([FromRoute] int snackbarId)
+        {
+            bool success = new SnackbarRepository().Delete(snackbarId);
+            return RedirectToPage(nameof(Index));
+        }
+
+        public IActionResult OnPostCancel()
+        {
+            return RedirectToPage(nameof(Index));
         }
     }
 }
