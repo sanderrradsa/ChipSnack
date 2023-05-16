c99c66f498603509719fa61043cc88f402e12f08
Sander Burger <sander.burger@student.nhlstenden.com>
2023-05-16 10:49:03 +0200

    CRUD Snackbar

---
+++ WebdevProjectStarterTemplate/Pages/Snackbars/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebdevProjectStarterTemplate.Pages.Snackbars
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebdevProjectStarterTemplate/Pages/Snackbars/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebdevProjectStarterTemplate.Pages.Snackbars
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
