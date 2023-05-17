d345f742713a2f5b486bd109dabef8e9f3636b91
Sander Burger <sander.burger@student.nhlstenden.com>
2023-05-17 09:55:38 +0200

    Snack CRUD files

---
+++ WebdevProjectStarterTemplate/Pages/Snacks/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebdevProjectStarterTemplate.Pages.Snacks
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
