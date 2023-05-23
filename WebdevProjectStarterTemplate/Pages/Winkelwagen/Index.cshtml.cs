45ff32b721cd494ea36083aa9881e4b56ab12f96
sander burger <sande@DESKTOP-TTFT1VN>
2023-05-23 14:34:54 +0200

    Winkelwagen product kunnen verwijderen en aantal aanpassen

--- WebdevProjectStarterTemplate/Pages/Winkelwagen/Index.cshtml.cs
+++ WebdevProjectStarterTemplate/Pages/Winkelwagen/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
 using WebdevProjectStarterTemplate.Models;
 using WebdevProjectStarterTemplate.Repositories;
 
@@ -8,13 +9,30 @@ namespace WebdevProjectStarterTemplate.Pages.Winkelwagen
     public class Index : PageModel
     {
         public IEnumerable<Bestelling> Bestelling { get; set; } = null!;
+
+        public int year { get; set; }
+        public int week { get; set; }
         public void OnGet()
         {
-            Bestelling = new BestellingRepository().GetBestellingWithSnack(1);
+            var dt = DateTime.Today;
+            Calendar cal = new CultureInfo("en-US").Calendar;
+            week = cal.GetWeekOfYear(dt, CalendarWeekRule.FirstDay, DayOfWeek.Monday) -1;
+            string date = DateTime.Now.ToString("yyyy");
+            year = Convert.ToInt32(date);
+            Bestelling = new BestellingRepository().GetBestellingWithSnack(1, year, week);
         }
+
         public IActionResult OnPostIncrement(int bestellingId)
         {
                 var updatedOrder = new BestellingRepository().Update(1, bestellingId);
+            OnGet();
+
+            return Page();
+
+        }
+        public IActionResult OnPostDecrement(int bestellingId)
+        {
+            var updatedOrder = new BestellingRepository().Update(-1, bestellingId);
             OnGet();
 
             return Page();
--- WebdevProjectStarterTemplate/Repositories/BestellingRepository.cs
+++ WebdevProjectStarterTemplate/Repositories/BestellingRepository.cs
@@ -11,10 +11,33 @@ namespace WebdevProjectStarterTemplate.Repositories
         {
             return new DbUtils().GetDbConnection();
         }
-
-        public IEnumerable<Bestelling> GetBestellingWithSnack(int gebruikerId)
+        public IEnumerable<Bestelling> Get()
         {
-            string sql = @"SELECT * FROM bestelling as b JOIN snack as s ON b.snackId = s.id WHERE gebruikerId = @gebruikerId";
+            string sql = "SELECT * FROM bestelling";
+
+            using var connection = GetConnection();
+            var bestellingen = connection.Query<Bestelling>(sql);
+            return bestellingen;
+        }
+        public Bestelling Get(int bestellingId)
+        {
+            string sql = "SELECT * FROM bestelling WHERE id = @bestellingId";
+
+            using var connection = GetConnection();
+            var bestelling = connection.QuerySingle<Bestelling>(sql, new { bestellingId });
+            return bestelling;
+        }
+        public bool Delete(int bestellingId)
+        {
+            string sql = @"DELETE FROM bestelling WHERE id = @bestellingId";
+
+            using var connection = GetConnection();
+            int numOfEffectedRows = connection.Execute(sql, new { bestellingId });
+            return numOfEffectedRows == 1;
+        }
+        public IEnumerable<Bestelling> GetBestellingWithSnack(int gebruikerId, int year, int week)
+        {
+            string sql = @"SELECT * FROM bestelling as b JOIN snack as s ON b.snackId = s.id WHERE gebruikerId = @gebruikerId AND jaar = @year AND weeknr = @week";
 
             using var connection = GetConnection();
             var BestellingWithSnack = connection.Query<Bestelling, Snack, Bestelling>(
@@ -24,22 +47,22 @@ namespace WebdevProjectStarterTemplate.Repositories
                     bestelling.Snack = snack;
                     return bestelling;
                 },
-                            new { gebruikerId },
+                            new { gebruikerId, year, week},
                 splitOn: "snackId"
             );
             return BestellingWithSnack;
         }
-        public Bestelling Update(int aantall, int bestellingId)
+        public Bestelling Update(int aantal, int bestellingId)
         {
 
             string sql = @"
                 UPDATE bestelling SET 
-                    aantal = aantal + @aantall
+                    aantal = aantal + @aantal
                 WHERE id = @bestellingId;
                 SELECT * FROM bestelling WHERE id = @bestellingId";
 
             using var connection = GetConnection();
-            var updatedOrder = connection.QuerySingle<Bestelling>(sql, new { Aantal = aantall, Id = bestellingId });
+            var updatedOrder = connection.QuerySingle<Bestelling>(sql, new { aantal, bestellingId });
             return updatedOrder;
         }
 
