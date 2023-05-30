c93820c30c26d8225768dc72d1bd0c9811bf9036
jorrit <jorritdehaan200@gmail.com>
2023-05-30 11:01:44 +0200

    SQL files toegevoegd (handig als je de database vern-- nouja)

---
+++ WebdevProjectStarterTemplate/Database_Setup/dummydata_core.sql
@@ -0,0 +1,7 @@
+-- Handgeschreven op 30/05/2023
+-- Versie 1.0
+
+-- Dit document INSERT data in de database die esentieel is voor het functioneren van het programma
+-- Dit document gaat er vanuit dat de database leeg is
+
+INSERT INTO budget VALUE (750);
