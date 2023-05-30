cecd433d93c148ee9b0a9aca94ad69a28c4eb0f8
sander burger <sande@DESKTOP-TTFT1VN>
2023-05-30 11:28:37 +0200

    alles fixen in main

--- WebdevProjectStarterTemplate/Pages/Snackbars/Create.cshtml.cs
+++ WebdevProjectStarterTemplate/Pages/Snackbars/Create.cshtml.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebdevProjectStarterTemplate.Models;
+using WebdevProjectStarterTemplate.Repositories;
 
 namespace WebdevProjectStarterTemplate.Pages.Snackbars
 {
     public class CreateModel : PageModel
     {
+        [BindProperty] public Snackbar Snackbar { get; set; } = null!;
+    
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+        
+            var createdCategory = new SnackbarRepository().Add(Snackbar);
+            return RedirectToPage(nameof(Index));
+        }
+
+        public IActionResult OnPostCancel()
+        {
+            return Redirect(nameof(Index));
         }
     }
 }
