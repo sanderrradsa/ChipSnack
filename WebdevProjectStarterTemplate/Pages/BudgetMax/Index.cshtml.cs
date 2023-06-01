6f5d46b49c5d59d69ccfaf7d5b28a5ad090545e6
sander burger <sande@DESKTOP-TTFT1VN>
2023-06-01 14:30:42 +0200

    Budget Index and update

---
+++ WebdevProjectStarterTemplate/Pages/BudgetMax/Index.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebdevProjectStarterTemplate.Models;
+using WebdevProjectStarterTemplate.Repositories;
+
+namespace WebdevProjectStarterTemplate.Pages.BudgetMax
+{
+    public class IndexModel : PageModel
+    {
+        public IEnumerable<Budget> budgets { get; set; } = null!;
+
+        public void OnGet()
+        {
+            budgets = new BudgetRepository().Get();
+        }
+    }
+}
---
+++ WebdevProjectStarterTemplate/Pages/BudgetMax/Update.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebdevProjectStarterTemplate.Models;
+using WebdevProjectStarterTemplate.Repositories;
+
+namespace WebdevProjectStarterTemplate.Pages.BudgetMax
+{
+    public class Update : PageModel
+    {
+        public Budget budget { get; set; } = null!;
+
+        public void OnGet(int budgetId)
+        {
+            budget = new BudgetRepository().Get(budgetId);
+        }
+
+        public IActionResult OnPost(Budget budget)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var updatedBudget = new BudgetRepository().Update(budget);
+
+            return RedirectToPage(nameof(Index));
+        }
+
+        public IActionResult OnPostCancel()
+        {
+            return RedirectToPage(nameof(Index));
+        }
+    }
+}
