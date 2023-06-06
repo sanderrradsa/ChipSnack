d94917b00d609a7399c45eee3d5eb9086bcedb46
jorrit <jorritdehaan200@gmail.com>
2023-06-06 12:54:23 +0200

    bas troep fix?

--- WebdevProjectStarterTemplate/Pages/Snacks/Index.cshtml.cs
+++ WebdevProjectStarterTemplate/Pages/Snacks/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebdevProjectStarterTemplate.Models;
 using WebdevProjectStarterTemplate.Repositories;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WebdevProjectStarterTemplate.Pages.Snacks
 {
