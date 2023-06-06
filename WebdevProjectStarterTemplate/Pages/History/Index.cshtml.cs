655c9502f1550fc87aa3843a3f61522d47f2d45d
jorrit <jorritdehaan200@gmail.com>
2023-06-06 18:41:51 +0200

    dingen die ik even snel in de bus heb gedaan

--- WebdevProjectStarterTemplate/Pages/History/Index.cshtml.cs
+++ WebdevProjectStarterTemplate/Pages/History/Index.cshtml.cs
@@ -4,21 +4,24 @@ using WebdevProjectStarterTemplate.Models;
 
 namespace WebdevProjectStarterTemplate.Pages.History;
 
+public enum TijdsRelativiteit
+{
+    Tijdens,
+    Voor,
+    Na
+}
+
 public class Index : PageModel
 {
     public IEnumerable<Models.History> History;
     public IEnumerable<int> BeschikbareJaren;
     public IEnumerable<int>? BeschikbareWeken;
-
+    
     public int? geselecteerdJaar;
     public int? geselecteerdeWeek;
+    public TijdsRelativiteit? GeselecteerdeRelativiteit;
     
-    public enum TijdsRelativiteit
-    {
-        Tijdens,
-        Voor,
-        Na
-    }
+
     public void OnGet(int? jaar, int? week, TijdsRelativiteit? relativiteit)
     {
         BeschikbareJaren = new HistoryRepository().GetAvailableYears();
@@ -26,6 +29,16 @@ public class Index : PageModel
         {
             geselecteerdJaar = jaar;
             BeschikbareWeken = new HistoryRepository().GetAvailableWeeks((int)jaar);
+        }
+
+        if (week is not null)
+        {
+            geselecteerdeWeek = week;
+        }
+
+        if (relativiteit is not null)
+        {
+            GeselecteerdeRelativiteit = relativiteit;
         }
         History = new HistoryRepository().Get();
         Console.WriteLine(relativiteit);
