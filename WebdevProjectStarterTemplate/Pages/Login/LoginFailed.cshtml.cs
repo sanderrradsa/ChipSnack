488aa01e50b81af0d8d943a083aad8e1bd16e5f7
basters-zz <bas.1997@hotmail.com>
2023-06-19 13:46:31 +0200

    Hashedpwdfix

--- WebdevProjectStarterTemplate/Pages/Login/LoginFailed.cshtml.cs
+++ WebdevProjectStarterTemplate/Pages/Login/LoginFailed.cshtml.cs
@@ -37,7 +37,7 @@ namespace WebdevProjectStarterTemplate.Pages.Login
                 using (var command = new MySqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Username", username);
-                    command.Parameters.AddWithValue("@Password", password);
+                    command.Parameters.AddWithValue("@Password", hashedPassword);
 
                     int count = Convert.ToInt32(command.ExecuteScalar());
                     string query1 = "SELECT * FROM gebruiker where email = @Username and rol = 1";
